697bc1e00708f62e452b2779df9b1f6065fb03e1
Azrael <azrael@mutualzz.com>
2025-12-05 22:56:30 -0800

    DecoratorWrapper: Added proper positioning

--- src/DecoratorWrapper/DecoratorWrapper.tsx
+++ src/DecoratorWrapper/DecoratorWrapper.tsx
@@ -1,16 +1,17 @@
 import { styled } from "@mutualzz/ui-core";
 
-const DecoratorWrapper = styled("span")(({
-    lineHeight: 0,
-    fontSize: "inherit",
-    color: "inherit",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    alignSelf: "center",
-    height: "100%",
-    flex: "0 0 auto",
-}));
+const DecoratorWrapper = styled("span")<{ position: "start" | "end" }>(
+    ({ position }) => ({
+        lineHeight: 0,
+        fontSize: "inherit",
+        color: "inherit",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: position === "start" ? "flex-start" : "flex-end",
+        height: "100%",
+        flex: "0 0 auto",
+    }),
+);
 
 DecoratorWrapper.displayName = "DecoratorWrapper";
 
