58c6882d1c263fa73da01185e2c09ae31dbe3a69
Azrael <azrael@mutualzz.com>
2025-12-05 21:26:15 -0800

    DecoratorWrapper: removed spacin g and position

--- src/DecoratorWrapper/DecoratorWrapper.tsx
+++ src/DecoratorWrapper/DecoratorWrapper.tsx
@@ -1,26 +1,15 @@
 import { styled } from "@mutualzz/ui-core";
 
-const DecoratorWrapper = styled("span")<{
-    position?: "start" | "end";
-    spacing?: string | number;
-}>(({ position, spacing = "0.5em" }) => ({
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    lineHeight: 1,
-
+const DecoratorWrapper = styled("span")(({
+    lineHeight: 0,
     fontSize: "inherit",
     color: "inherit",
-
-    ...(position === "start" && {
-        marginRight: spacing,
-    }),
-    ...(position === "end" && {
-        marginLeft: spacing,
-    }),
-
-    flexShrink: 0,
-    flexGrow: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    height: "100%",
+    flex: "0 0 auto",
 }));
 
 DecoratorWrapper.displayName = "DecoratorWrapper";
